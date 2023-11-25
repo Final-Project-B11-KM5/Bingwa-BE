38918d0da1303501ac822b43429e75ceae7cef13
shera alice <sheraalice@students.amikom.ac.id>
2023-11-25 17:09:14 +0700

    feat: add paggination course

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -4,10 +4,11 @@ const Auth = require("../middlewares/authentication");
 const checkRole = require("../middlewares/checkRole");
 
 router.post("/", Auth, checkRole(["admin"]), createCourse);
-router.get("/", Auth, checkRole(["admin"]), showAllCourse);
+// router.get("/", Auth, checkRole(["admin"]), showAllCourse); // USER nanti gabsia lihat kelas apa aja dong sblm login
+router.get("/", showAllCourse)
 router.get("/:idCourse", Auth, checkRole(["admin"]), detailCourse);
 router.put("/:idCourse", Auth, checkRole(["admin"]), editCourse);
 router.delete("/:idCourse", Auth, checkRole(["admin"]), deleteCourse);
-router.get("/course", getCourse);
+router.get("/", getCourse);
 
 module.exports = router;
