f8aa7966ef55d714a907cb82e347d508ba51c728
Hafid <hafid@gmail.com>
2023-11-25 14:01:46 +0700

    update

--- controllers/vidio.controllers.js
+++ controllers/vidio.controllers.js
@@ -4,7 +4,20 @@ const path = require("path");
 const imagekit = require("../libs/imagekit");
 
 module.exports = {
-    postVidio: async()=>{
-        
+  postVidio: async (req, res, next) => {
+    try {
+      const { idCourse } = req.body;
+      const file = req.file;
+      let vidioUrl = null;
+      if (!file) {
+        return res.status(400).json({
+          status: false,
+          message: "Vidio file is not found",
+          data: null,
+        });
+      }
+    } catch (err) {
+      next(err);
     }
-}
+  },
+};
--- prisma/migrations/20231123160153_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[courseName]` on the table `Course` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Course_courseName_key" ON "Course"("courseName");
