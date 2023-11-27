3e8f71166d06bf1ac212758044f71e5167e63c4b
Hafid <hafid@gmail.com>
2023-11-28 05:31:56 +0700

    update shcema:prisma

--- prisma/migrations/20231126062713_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Chapter" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Chapter_pkey" PRIMARY KEY ("id")
-);
