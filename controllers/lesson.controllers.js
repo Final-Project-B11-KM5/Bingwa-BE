690df6238e5e02048c9111d5ed499816ccc70fc7
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-11-27 07:26:11 +0700

    feat: add getAllLessons and getDetailLesson

--- controllers/lesson.controllers.js
+++ controllers/lesson.controllers.js
@@ -7,6 +7,12 @@ const findChapterById = async (chapterId) => {
   });
 };
 
+const findLessonById = async (lessonId) => {
+  return await prisma.lesson.findUnique({
+    where: { id: Number(lessonId) },
+  });
+};
+
 const createLesson = async (req, res, next) => {
   try {
     const { lessonName, videoURL, chapterId } = req.body;
@@ -35,6 +41,46 @@ const createLesson = async (req, res, next) => {
   }
 };
 
+const getAllLessons = async (req, res, next) => {
+  try {
+    const lessons = await prisma.lesson.findMany();
+
+    res.status(200).json({
+      status: true,
+      message: "Get all lessons successful",
+      data: { lessons },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getDetailLesson = async (req, res, next) => {
+  try {
+    const lessonId = req.params.id;
+
+    const lesson = await findLessonById(lessonId);
+
+    if (!lesson) {
+      return res.status(404).json({
+        status: false,
+        message: "Lesson not found",
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "Get detail lesson successful",
+      data: { lesson },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   createLesson,
+  getAllLessons,
+  getDetailLesson,
 };
--- routes/lesson.routes.js
+++ routes/lesson.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const { createLesson } = require("../controllers/lesson.controllers");
+const { createLesson, getAllLessons, getDetailLesson } = require("../controllers/lesson.controllers");
 const Auth = require("../middlewares/authentication");
 const checkRole = require("../middlewares/checkRole");
 
 router.post("/", Auth, checkRole(["admin"]), createLesson);
+router.get("/", getAllLessons);
+router.get("/:id", Auth, checkRole(["user", "admin"]), getDetailLesson);
 
 module.exports = router;
