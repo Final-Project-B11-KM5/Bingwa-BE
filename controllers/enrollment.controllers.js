8b8436000a61c2235f6a2b30071eeb8e2e4b13b0
Hafid <hafid@gmail.com>
2023-11-30 23:49:08 +0700

    add courseEnrollemnt

--- controllers/enrollment.controllers.js
+++ controllers/enrollment.controllers.js
@@ -3,13 +3,48 @@ const prisma = new PrismaClient();
 
 module.exports = {
   courseEnrollment: async (req, res, next) => {
+    //for free course
     try {
       const { courseId } = req.params;
-      const user = req.user;
+      const { id } = req.user;
       const course = await prisma.course.findUnique({
         where: {
-          id: courseId,
+          id: Number(courseId),
         },
+      });
+      if (!course) {
+        return res.status(404).json({
+          status: false,
+          message: `Course not found with id ${courseId}`,
+          data: null,
+        });
+      }
+      
+      const statusEnrollUser = await prisma.enrollment.findFirst({
+        where: {
+          courseId:Number(courseId),
+          userId: id,
+        },
+      });
+      // check user alredy enrol course or not
+      if (statusEnrollUser) {
+        return res.status(400).json({
+          status: false,
+          message: `User Alrady Enroll this COurse`,
+          data: null,
+        });
+      }
+      let enrollCourse = await prisma.enrollment.create({
+        data: {
+          isPaid: course.isPremium,
+          userId: id,
+          courseId: Number(courseId),
+        },
+      });
+      res.status(201).json({
+        status: true,
+        message: "Succes To Enroll Course",
+        data: enrollCourse,
       });
     } catch (err) {
       next(err);
