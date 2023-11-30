9535503a21b0a991f1a8a5648a559edea9051f6f
Hafid <hafid@gmail.com>
2023-11-30 08:53:52 +0700

    update controller and middleware

--- controllers/enrollment.controllers.js
+++ controllers/enrollment.controllers.js
@@ -2,5 +2,17 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = {
-    
-}
+  courseEnrollment: async (req, res, next) => {
+    try {
+      const { courseId } = req.params;
+      const user = req.user;
+      const course = await prisma.course.findUnique({
+        where: {
+          id: courseId,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
