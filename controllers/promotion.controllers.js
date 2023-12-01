caef364cfbf4cf64ab1e9db15040e0ce1e8c54a4
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-12-01 10:56:46 +0700

    feat: add GET endpoint for getPromotionById

--- controllers/promotion.controllers.js
+++ controllers/promotion.controllers.js
@@ -33,4 +33,30 @@ module.exports = {
       next(err);
     }
   },
+
+  getPromotionById: async (req, res, next) => {
+    try {
+      const promotionId = req.params.id;
+
+      const promotion = await prisma.promotion.findUnique({
+        where: { id: Number(promotionId) },
+      });
+
+      if (!promotion) {
+        return res.status(404).json({
+          status: false,
+          message: "Promotion not found",
+          data: null,
+        });
+      }
+
+      res.status(200).json({
+        status: true,
+        message: "Get detail promotion successful",
+        data: { promotion },
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
 };
--- routes/promotion.routes.js
+++ routes/promotion.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { createPromotion, getAllPromotions } = require("../controllers/promotion.controllers");
+const { createPromotion, getAllPromotions, getPromotionById } = require("../controllers/promotion.controllers");
 const Auth = require("../middlewares/authentication");
 const checkRole = require("../middlewares/checkRole");
 
-router.get("/", getAllPromotions);
-router.post("/", createPromotion);
+router.get("/", Auth, checkRole(["admin"]), getAllPromotions);
+router.post("/", Auth, checkRole(["admin"]), createPromotion);
+router.get("/:id", Auth, checkRole(["admin"]), getPromotionById);
 
 module.exports = router;
