9532bbe1e4406c5c1d30e40b47d552cf9cdbe7e7
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-12-02 13:34:37 +0700

    feat: Add endpoint for creating enrollment

--- routes/enrollment.routes.js
+++ routes/enrollment.routes.js
@@ -1,14 +1,10 @@
 const router = require("express").Router();
-const {
-  getAllEnrollment,
-  getDetailEnrollment,
-  courseEnrollment,
-} = require("../controllers/enrollment.controllers");
+const { getAllEnrollment, getDetailEnrollment, courseEnrollment } = require("../controllers/enrollment.controllers");
 const Auth = require("../middlewares/authentication");
 const checkRole = require("../middlewares/checkRole");
 
-router.post("/:courseId/course", Auth, courseEnrollment);
 router.get("/", Auth, checkRole(["user", "admin"]), getAllEnrollment);
 router.get("/:id", Auth, checkRole(["user", "admin"]), getDetailEnrollment);
+router.post("/:courseId/course", Auth, checkRole(["user", "admin"]), courseEnrollment);
 
 module.exports = router;
