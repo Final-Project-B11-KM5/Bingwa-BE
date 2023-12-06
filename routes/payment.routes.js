bb7adf70b3f4fe1cd09abfc1d2f0fdbd3a889209
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-12-06 14:01:07 +0700

    feat: add GET endpoint for getPaymentHistory

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { getAllPayments } = require("../controllers/payment.controllers");
+const { getAllPayments, getPaymentHistory } = require("../controllers/payment.controllers");
 const Auth = require("../middlewares/authentication");
 const checkRole = require("../middlewares/checkRole");
 
 router.get("/", Auth, checkRole(["admin"]), getAllPayments);
+router.get("/history", Auth, checkRole(["user", "admin"]), getPaymentHistory);
 
 module.exports = router;
