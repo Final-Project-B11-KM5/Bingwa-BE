81a3698fb6de384112bee65942c6f39ba624e2bc
Hafid <hafid@gmail.com>
2023-12-08 09:02:21 +0700

    update:payment routes

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const { getAllPayments, getPaymentHistory } = require("../controllers/payment.controllers");
+const { getAllPayments, getPaymentHistory,createPayment,getDetailPayment } = require("../controllers/payment.controllers");
 const Auth = require("../middlewares/authentication");
 const checkRole = require("../middlewares/checkRole");
 
+router.post("/:idCourse/course",Auth,checkRole(["user"]),createPayment);
+router.get("/:idCourse/course",getDetailPayment);
 router.get("/", Auth, checkRole(["admin"]), getAllPayments);
 router.get("/history", Auth, checkRole(["user", "admin"]), getPaymentHistory);
 
