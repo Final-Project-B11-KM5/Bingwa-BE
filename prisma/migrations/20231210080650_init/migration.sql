cc4d033a4e6b1b0579d4e82d2d71324672d47982
Hafid <hafid@gmail.com>
2023-12-12 21:46:24 +0700

    update prisma schema

--- prisma/migrations/20231210080650_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Enrollment" ADD COLUMN     "progres" DECIMAL(65,30) NOT NULL DEFAULT 0;
--- prisma/migrations/20231212025931_init/migration.sql
+++ prisma/migrations/20231212025931_init/migration.sql
@@ -5,11 +5,19 @@ ALTER TABLE "Enrollment" DROP CONSTRAINT "Enrollment_courseId_fkey";
 ALTER TABLE "Enrollment" DROP CONSTRAINT "Enrollment_userId_fkey";
 
 -- AlterTable
-ALTER TABLE "Enrollment" ALTER COLUMN "userId" DROP NOT NULL,
+ALTER TABLE "Enrollment" ADD COLUMN     "progres" DECIMAL(65,30) NOT NULL DEFAULT 0,
+ADD COLUMN     "userRating" INTEGER,
+ALTER COLUMN "userId" DROP NOT NULL,
 ALTER COLUMN "courseId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Tracking" ADD COLUMN     "courseId" INTEGER;
 
 -- AddForeignKey
 ALTER TABLE "Enrollment" ADD CONSTRAINT "Enrollment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Enrollment" ADD CONSTRAINT "Enrollment_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Tracking" ADD CONSTRAINT "Tracking_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE SET NULL ON UPDATE CASCADE;
